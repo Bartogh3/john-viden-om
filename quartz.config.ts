3fde4ddf5cbe006fe4a05ddd5b57ee7324122e28
Bartogh3 <bartogh@outlook.dk>
2026-04-03 00:40:20 +0200

    vault backup: 2026-04-03 00:40:20

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "John Viden Om",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "da-DK",
+    baseUrl: "bartogh3.github.io/john-viden-om",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
